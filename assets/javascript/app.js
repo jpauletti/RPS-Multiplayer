8ee506542faf8e12233c6adcddafbaafcb21c8a9
Julia Pauletti <julia.pauletti@yahoo.com>
2019-04-18 10:13:38 -0500

    basic setup, js WIP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,6 +18,17 @@ var connections = database.ref("/connections");
 // updated when the client's connections state changes - boolean value - true if connected, false if not
 var connected = database.ref(".info/connected");
 
+// player info
+var players = database.ref("/players");
+
+// whose turn it is
+var turn = database.ref("/turn");
+
+var playerOne = null;
+var playerTwo = null;
+var yourPlayer = "";
+
+var numOfPlayers = 0;
 // listen to if there is a connection
 connected.on("value", function(snapshot) {
     // if connected
@@ -26,6 +37,18 @@ connected.on("value", function(snapshot) {
 
         // add user to the connections list
         var conn = connections.push(true);
+
+        // if (numOfPlayers <= 2) {
+        //     // add user to the connections list
+        //     var conn = connections.push(true);
+        //     numOfPlayers++;
+        // } 
+        // else {
+        //     numOfPlayers = 0;
+        //     var i = 0;
+        //     database.ref("/connections" + i)
+        //     numO
+        // }
 
         // remove user when they disconnect
         conn.onDisconnect().remove();
@@ -38,20 +61,88 @@ connected.on("value", function(snapshot) {
 
 connections.on("child_added", function(snapshot) { // do I need this????
     console.log("child added");
+    console.log(snapshot.key);
+    var key = snapshot.key;
+
+    // playerOne = snapshot.key;
+    // console.log(playerOne);
+
+    if (!playerOne) {
+        playerOne = key;
+        yourPlayer = "playerOne";
+        players.push({
+            id: key,
+            name: yourPlayer,
+            wins: 0,
+            losses: 0,
+            ties: 0
+        });
+    } else {
+        playerTwo = key;
+        yourPlayer = "playerTwo";
+        players.push({
+            id: key,
+            name: yourPlayer,
+            wins: 0,
+            losses: 0,
+            ties: 0
+        });
+    }
+    
 });
 
 connections.on("value", function (snapshot) {
     var numChildren = snapshot.numChildren();
+    console.log("value watch: " + snapshot.key);
+
+    // if () {}
 
     if (numChildren > 2) {
         console.log("more than 2 people trying to play");
+        // code here to hide playing section - show user a "players full - try later" message
+
+
         // show sections to play here
+    } else if (numChildren === 1) {
+        console.log("only one person here");
+        // message about "wait for another person to join"
+
     } else {
         console.log("<= 2 people - let's play!");
-        // code here to hide playing section - show user a "players full - try later" message
+        // loop through and assign each to player1 or player2
+        snapshot.forEach(function (childSnapshot) {
+            var key = childSnapshot.key;
+
+            if (!playerOne) {
+                playerOne = key;
+                yourPlayer = key;
+                players.push({
+                    id: key,
+                    name: "",
+                    wins: 0,
+                    losses: 0,
+                    ties: 0
+                });
+            } else {
+                playerTwo = key;
+                yourPlayer = key;
+                players.push({
+                    id: key,
+                    name: "",
+                    wins: 0,
+                    losses: 0,
+                    ties: 0
+                });
+            }
+
+            console.log("player1: " + playerOne);
+            console.log("playertwo: " + playerTwo);
+        });
     }
 
 });
+
+// connections.child(playerOne).on();
 
 
 
@@ -61,107 +152,60 @@ connections.on("value", function (snapshot) {
 
 
 // // ==================================================================================================
-// // from practice exercise
+// RPS GAME
 
-// // Create a variable to reference the database.
-// var database = firebase.database();
+// know when 2 people have joined the game
+// after that, every following group of 2 gets a new game created for them
 
-// // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-// // connectionsRef references a specific location in our database.
-// // All of our connections will be stored in this directory.
-// var connectionsRef = database.ref("/connections");
-
-// // '.info/connected' is a special location provided by Firebase that is updated every time
-// // the client's connection state changes.
-// // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-// var connectedRef = database.ref(".info/connected");
-
-// // When the client's connection state changes...
-// connectedRef.on("value", function (snap) {
-
-//     // If they are connected..
-//     if (snap.val()) {
-
-//         // Add user to the connections list.
-//         var con = connectionsRef.push(true);
-
-//         // Remove user from the connection list when they disconnect.
-//         con.onDisconnect().remove();
-//     }
-// });
-
-// // When first loaded or when the connections list changes...
-// connectionsRef.on("value", function (snapshot) {
-
-//     // Display the viewer count in the html.
-//     // The number of online users is the number of children in the connections list.
-//     $("#watchers").text(snapshot.numChildren());
-// });
-
-// // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-// // Set Initial Counter
-// var initialValue = 100;
-// var clickCounter = initialValue;
-
-// // At the page load and subsequent value changes, get a snapshot of the local data.
-// // This callback allows the page to stay updated with the values in firebase node "clicks"
-// database.ref("/clicks").on("value", function (snapshot) {
-
-//     // Print the local data to the console.
-//     console.log(snapshot.val());
+    // know when each player clicks on rock, paper, or scissors and save their choice
 
 
-//     // Change the HTML to reflect the local value in firebase.
-//     clickCounter = snapshot.val().clickCount;
+// hover on image, show text
+var test = "";
+$("img").on("mouseenter mouseleave", function () {
+    test = $(this);
+    var id = $(this).attr("id");
+    console.log($(this).parent().parent().next().children(".choice-" + id).html());
+    $(this).parent().parent().next().children(".choice-" + id).children().toggleClass("hide");
+})
 
-//     // Log the value of the clickCounter
-//     console.log(clickCounter);
 
-//     // Change the HTML to reflect the local value in firebase.
-//     $("#click-value").text(clickCounter);
 
-//     // If any errors are experienced, log them to console.
-// }, function (errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-// });
+var app = {
+    $rock: $("#rock"),
+    $paper: $("#paper"),
+    $scissors: $("#scissors")
+};
 
-// // --------------------------------------------------------------
+app.$rock.on("click", function () {
+    console.log("rock");
 
-// // Whenever a user clicks the click-button
-// $("#click-button").on("click", function () {
+    if (turn === yourPlayer) {
+        // save choice
+        // go to next turn
+    } else {
+        // nothing bc not your turn
+    }
+})
 
-//     // Reduce the clickCounter by 1
-//     clickCounter--;
+app.$paper.on("click", function () {
+    console.log("paper");
+});
 
-//     // Alert User and reset the counter
-//     if (clickCounter === 0) {
-//         alert("Phew! You made it! That sure was a lot of clicking.");
-//         clickCounter = initialValue;
-//     }
+app.$scissors.on("click", function () {
+    console.log("scissors");
+});
 
-//     // Save new value to Firebase
-//     database.ref("/clicks").set({
-//         clickCount: clickCounter
-//     });
 
-//     // Log the value of clickCounter
-//     console.log(clickCounter);
-// });
+// if (connections.child(playerOne))
+// console.log(connections.hasChild(playerOne));
+// console.log(database.child(playerOne));
 
-// // Whenever a user clicks the restart button
-// $("#restart-button").on("click", function () {
 
-//     // Set the clickCounter back to initialValue
-//     clickCounter = initialValue;
+// if player 1 wins (all possible ways),
+// p1wins++
 
-//     // Save new value to Firebase
-//     database.ref("/clicks").set({
-//         clickCount: clickCounter
-//     });
+// if player 2 wins (all possible ways),
+// p2wins++
 
-//     // Log the value of clickCounter
-//     console.log(clickCounter);
-
-//     // Change the HTML Values
-//     $("#click-value").text(clickCounter);
-// });
+// otherwise, it's a tie
